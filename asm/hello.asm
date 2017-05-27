b19491b5da9ff1f11e33a6f4feaf3a39da6f7d95
kirk <kirk91.han@gmail.com>
2017-05-27 14:01:02 +0800

    add asm codes

---
+++ asm/hello.asm
@@ -0,0 +1,16 @@
+global start
+
+section .data
+    msg db  "hello, world"
+    .len: equ $ - msg
+
+section .text
+start:
+    mov rax, 0x2000004
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, msg.len
+    syscall
+    mov rax, 0x2000001
+    mov rdi, 0
+    syscall
---
+++ asm/sum.asm
@@ -0,0 +1,48 @@
+section .data
+    ; Define constants
+    num1:   equ 100
+    num2:   equ 50
+    ; initialize messages
+    msg:    db "Sum is correct\n"
+
+section .text
+    global _start
+
+;; entry point
+_start:
+    ; set num1's value to rax
+    mov rax, num1
+    ; set num2's value to rbx
+    mov rbx, num2
+    ; get sum of rax and rbx, and store it's value in rax
+    add rax, rbx
+    ; compare rax and 150
+    cmp rax, 150
+    ; go to .exit label if rax and 150 are not equal
+    jne .exit
+    ; go to .righSum lable if rax and 150 are equal
+    jmp .righSum
+
+; Print message that sum is correct
+.righSum:
+    ;; write syscall
+    mov rax, 1
+    ;; file descriptor, standard output
+    mov rdi, 1
+    ;; message address
+    mov rsi, msg
+    ;; length of message
+    mov rdx, 15
+    ;; call write syscall
+    syscall
+    ; exit from program
+    jmp .exit
+
+; exit procedure
+.exit:
+    ; exit syscall
+    mov rax, 60
+    ; exit code
+    mov rdi, 0
+    ; call exit syscall
+    syscall
